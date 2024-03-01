a55678e65d1d6935d811327e21cfc441f3354917
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-29 18:12:31 -0800

    CHORE: add helper.h

---
+++ inc/helper.h
@@ -0,0 +1,12 @@
+#include "header.h"
+
+// function prototypes
+int my_strlen(char* str);
+char *my_strncpy(char* dst, char* src, int n);
+char *my_strcpy(char* dst, char* src);
+char *my_strcat(char *dst, char *src);
+char* my_strchr(char* str_1, char char_1);
+int my_strchr_index(char* str_1, char char_1);
+void my_putstr(char* str);
+void my_putstrerr(char* str);
+// bool my_isalnum(char c);
