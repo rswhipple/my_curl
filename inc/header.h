d3ecfd80753b7c1e16830e8c320fdc93e0492725
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-29 18:12:23 -0800

    CHORE: add header.h

---
+++ inc/header.h
@@ -0,0 +1,11 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+
+// function prototypes
+int my_curl(char* url);
