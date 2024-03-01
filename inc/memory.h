af7a25e25fa585067c6905d0561686ced4603b0d
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-29 18:12:38 -0800

    CHORE: add memory.h

---
+++ inc/memory.h
@@ -0,0 +1,7 @@
+#include "header.h"
+
+// function prototypes
+char *init_str(int size);
+char** init_string_arr(int size);
+void free_string_arr(char** string_array);
+void print_array(char** array);
